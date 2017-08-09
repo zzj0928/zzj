5962fc8d30b65fc82622d36353a22fe46c18b640
zzjtest2017 <zhangzhanjin2006@163.com>
2017-08-09 23:36:21 +0800

    add

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	'index_router'=>'index',
+
+	);
+
+// class CONFIG extends Base{
+// 	var $index_router;
+
+// 	public function Ini($value=''){
+// 		$this->index_router = 'index';
+// 	}
+// }
